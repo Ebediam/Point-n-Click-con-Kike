951c288c68169d57c982c4192250284531cea21f
Ebediam <felipebiosca@gmail.com>
2020-10-15 17:13:05 +0200

    Update 1: Barebones character controller

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform character;
+    public delegate void MouseClickDelegate();
+
+    public static MouseClickDelegate OnLeftClickEvent;
+    public static MouseClickDelegate OnRightClickEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnLeftClickEvent?.Invoke();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            OnRightClickEvent?.Invoke();
+        }
+
+    }
+
+
+    public static Vector3 MousePosition()
+    {
+        Vector3 charPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        charPos -= charPos.z * new UnityEngine.Vector3(0, 0, 1);
+        return charPos;
+    }
+
+}
---
+++ Assets/MouseFollow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class MouseFollow : MonoBehaviour
+{
+    public bool active;
+    public float speed;
+    public Vector3 goal;
+    public float proximitySensitivity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.OnRightClickEvent += MoveTowards;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+
+    void MoveTowards()
+    {
+        goal = GameManager.MousePosition();
+
+        if (active)
+        {
+            return;
+        }
+
+        active = true;
+        StartCoroutine(MoveTowardsCoroutine());
+    }
+
+    IEnumerator MoveTowardsCoroutine()
+    {
+        while((Vector3.Distance(transform.position, goal) > proximitySensitivity))
+        {
+            transform.position += (goal - transform.position).normalized * speed*Time.deltaTime;
+                
+                //Vector3.Lerp(transform.position, goal, speed);
+
+            yield return null;
+
+        }
+
+        active = false;
+
+    }
+}
